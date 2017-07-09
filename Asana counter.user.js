bc20d5dfcb7b4603eb74f4b7f16567dc332e47a5
Vladyslav Sitalo <root@stvad.org>
2017-07-09 18:53:26 +0100

    updated working version

--- Asana counter.user.js
+++ Asana counter.user.js
@@ -5,7 +5,7 @@
 // @include     https://app.asana.com/*
 // @version     1
 // @grant       none
-// @run-at      document-idle
+// @run-at      document-end
 // @require     https://code.jquery.com/jquery-3.2.1.min.js
 // @require     https://craig.global.ssl.fastly.net/js/mousetrap/mousetrap.min.js
 // @require     https://raw.githubusercontent.com/uzairfarooq/arrive/master/minified/arrive.min.js
@@ -17,13 +17,11 @@ var selectedRows = {};
 (function() {
     legacyMethodSetup();
 
-    // window.selectedRows = {};
     integratedMethodSetup();
 })();
 
 function integratedMethodSetup() {
     var task_rows = document.querySelectorAll("#grid tr,.TaskList .itemRow");
-    console.log(task_rows);
     var taskObserver = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
             processRowEvent(mutation.target);
@@ -31,74 +29,33 @@ function integratedMethodSetup() {
     });
 
     function observeRow(row) {
-        //this contraption needed to accomodate 2 types of rows that we can receive
-        //tr and .itemRow
+        //this contraption needed to accomodate 2 types of rows that we can receive //tr and .itemRow
         taskObserver.observe($(row).find(".itemRow").addBack()[0], {attributeFilter: ["class"]});
     }
 
     task_rows.forEach(observeRow);
 
-    // var gridObserver = new MutationObserver(function(mutations) {
-    //     mutations.forEach(function(mutation) {
-    //         mutation.addedNodes.forEach(observeRow);
-    //     });
-    // });
-    //
-    // // gridObserver.observe($("#grid:first tbody,.TaskList")[0], {childList: true});
-    //
-    //
-    // //id center_pane__contents
-    // //class column-contents-on-click-below-content
-    // var centerContentObserver = new MutationObserver(function (mutations) {
-    //     mutations.forEach(function (mutation) {
-    //         console.log($(mutation.target));
-    //         console.log($(mutation.target).find("#grid tbody,.TaskList"));
-    //         var tableSelector = $(mutation.target).find("#grid tbody,.TaskList");
-    //         var grid = $(mutation.target).find("#grid tbody,.TaskList")[0];
-    //         console.log(grid);
-    //         if (!tableSelector.length) {
-    //             return;
-    //         }
-    //         gridObserver.observe(grid, {childList: true});
-    //         rows = grid.querySelectorAll("tr,.itemRow");
-    //         rows.forEach(observeRow);
-    //
-    //         runningSum = 0;
-    //         selectedRows = {};
-    //
-    //     });
-    // });
-    //
-    // centerContentObserver.observe($("#center_pane__contents:first .column-contents-on-click-below-content")[0],
-    //     {childList: true});
-
-    // var centerContentVerboseObserver = new MutationObserver(function (mutations) {
-    //     mutations.forEach(function (mutation) {
-    //         console.log(mutation.target);
-    //     });
-    // });
-    //
-    // centerContentVerboseObserver.observe($("#center_pane__contents:first .column-contents-on-click-below-content")[0],
-    //     {attributeFilter: ["class"], subtree: true, attributes: true});
-
     $("#center_pane__contents:first .column-contents-on-click-below-content").
     arrive("#grid tr,.TaskList .itemRow", function (newElement) {
-        console.log(newElement);
+        observeRow(newElement);
+    });
+
+    $("#center_pane__contents:first .column-contents-on-click-below-content").
+    arrive("#grid,.TaskList", function (newElement) {
+        // When change project/view - deselection is not happening, so we need to cleanup ourselves.
+        runningSum = 0;
+        selectedRows = {};
     });
 
 }
 
 function processRowEvent(row) {
     var textArea = $(row).find("textarea");
-    console.log(textArea.val());
+    // console.log(textArea.val());
     var isSelected = $(row).is(".grid-row-selected,.itemRow--highlighted,.itemRow--focused");
-    console.log("isSelected: ", isSelected);
+    // console.log("isSelected: ", isSelected);
     taskNum = getNumberFromTaskName(textArea.val());
-    // dictKey = row.id;
     dictKey = textArea[0].id;
-    console.log(textArea);
-    console.log(dictKey);
-    console.log(selectedRows);
     if (dictKey in selectedRows) {
         if (isSelected) {
             // handles the case when we change the value on the task, while it has stayed selected
@@ -114,7 +71,7 @@ function processRowEvent(row) {
         selectedRows[dictKey] = taskNum;
         runningSum += taskNum;
     }
-    console.log(runningSum);
+    // console.log(runningSum);
     if (Object.keys(selectedRows).length > 1) {
         displayResult(runningSum);
     }
